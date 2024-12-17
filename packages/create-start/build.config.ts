988f5622d14075f9ed980668c161a757e635fee7
timoconnellaus <tim.oconnell.australia@gmail.com>
2024-12-17 11:32:30 +1100

    fix build

--- packages/create-start/build.config.ts
+++ packages/create-start/build.config.ts
@@ -1,13 +1,21 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-  entries: ['src/index', 'src/cli-entry'],
+  entries: [
+    {
+      builder: 'mkdist',
+      input: './src/',
+      pattern: ['**/*.{ts,tsx}', '!**/template/**'],
+    },
+  ],
+  outDir: './dist',
   clean: true,
+  declaration: true,
   rollup: {
     inlineDependencies: true,
     esbuild: {
       target: 'node18',
-      minify: true,
+      minify: false,
     },
   },
 })
